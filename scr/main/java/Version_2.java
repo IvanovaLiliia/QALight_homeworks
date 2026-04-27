95eb7b71458ef220a36f8b5dd33e4e040e3e32ae
Liliia71504 <53265693+Liliia71504@users.noreply.github.com>
2026-04-27 22:34:06 +0300

    Second version of my Java Code

---
+++ scr/main/java/Version_2.java
@@ -0,0 +1,14 @@
+public class Version_2 {
+
+    public static void main(String[] args) {
+
+        int [] numbers = {1, 9, 2, 5, 7};
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println(sum);
+    }
+    }
+
