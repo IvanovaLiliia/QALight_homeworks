ac68070e9a108a3c10de2cd5891d4a7bb12504d7
Liliia71504 <53265693+Liliia71504@users.noreply.github.com>
2026-05-28 15:27:55 +0300

    Homework18.IvanovaLiliia

--- scr/test/java/aqa_lecture_6/LogInTest.java
+++ scr/test/java/aqa_lecture_6/LogInTest.java
@@ -1,5 +1,6 @@
 package aqa_lecture_6;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LogInTest extends BaseTest {
@@ -8,6 +9,7 @@ public class LogInTest extends BaseTest {
     public void LogInNegative() {
 
         String email = "admin@test.com";
+        String RemindText = "Зміна паролю";
 
         HomePage homePage = new HomePage(getDriver());
         homePage.clickOnLogInPage();
@@ -15,6 +17,13 @@ public class LogInTest extends BaseTest {
         LogInPage logInPage = new LogInPage(getDriver());
         logInPage.EnterEmail(email);
         logInPage.clickOnForgetPassword();
+
+        ChangePasswordPage changePasswordPage = new ChangePasswordPage(getDriver());
+        String actualResultValue = changePasswordPage.getTitle();
+
+        System.out.println("Title on the  page: " + actualResultValue);
+        Assert.assertTrue(actualResultValue.contains(RemindText),
+                "Page title does not contain the search word!");
 
     }
 
